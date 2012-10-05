0f4ad612fe58a3da870b8d3e8daefd027d9427c1
DAGr8 <renocustom@gmail.com>
2012-10-05 12:26:50 -0400

    Bringing Paranoid Android Features to AOKP (3/5)
        All Credits to molesarecoming and the whole PARANOID ANDROID team
        Please understand it is a very dirty hack and pushing every PA commit
        one by one to preserve original credit was impossible but this deserve all
        of Paranoid android team's credit for their source. I am just sharing it .
    
    Change-Id: I0c74ffc1fbcbaf37ec1ab356d61576dfe6ec5932

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -230,6 +230,7 @@ static struct fs_path_config android_files[] = {
     { 00440, AID_BLUETOOTH, AID_BLUETOOTH, "system/etc/bluetooth/network.conf" },
     { 00444, AID_NET_BT,    AID_NET_BT,    "system/etc/bluetooth/blacklist.conf" },
     { 00640, AID_SYSTEM,    AID_SYSTEM,    "system/etc/bluetooth/auto_pairing.conf" },
+    { 00644, AID_SYSTEM,    AID_SYSTEM,    "system/etc/paranoid/properties.conf" },
     { 00444, AID_RADIO,     AID_AUDIO,     "system/etc/AudioPara4.csv" },
     { 00555, AID_ROOT,      AID_ROOT,      "system/etc/ppp/*" },
     { 00555, AID_ROOT,      AID_ROOT,      "system/etc/rc.*" },
